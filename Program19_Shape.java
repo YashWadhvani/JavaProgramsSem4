ed1574bd33d3f72b76e30968da450a50755e0eed
YashWadhvani <yashwadhvani123@gmail.com>
2024-01-12 00:24:53 +0530

    Program 19 Done

--- Program19_Shape.java
+++ Program19_Shape.java
@@ -17,15 +17,85 @@ class Circle extends Shape {
         this.rad = rad;
     }
 
-    double perimeter(float rad) {
-        return (2 * Math.PI * rad);
+    @Override
+    double perimeter() {
+        return (2 * Math.PI * this.rad);
     }
 
-    double area(double rad) {
-        return (Math.PI * Math.pow(rad, 2));
+    @Override
+    double area() {
+        return (Math.PI * Math.pow(this.rad, 2));
+    }
+
+    void displayMetrics() {
+        System.out.println(String.format("Radius of Circle= %.2f", this.rad));
+        System.out.println(String.format("Perimeter of Circle = %.2f", this.perimeter()));
+        System.out.println(String.format("Area of Circle = %.2f", this.area()));
+        System.out.println();
+    }
+}
+
+class Rectangle extends Shape {
+    double height, width;
+
+    Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    double perimeter() {
+        return (2 * (this.height + this.width));
+    }
+
+    @Override
+    double area() {
+        return (this.height * this.width);
+    }
+
+    void displayMetrics() {
+        System.out.println(String.format("Height of Rectangle = %.2f", this.height));
+        System.out.println(String.format("Width of Rectangle = %.2f", this.width));
+        System.out.println(String.format("Perimeter of Rectangle = %.2f", this.perimeter()));
+        System.out.println(String.format("Area of Rectangle = %.2f", this.area()));
+        System.out.println();
+    }
+}
+
+class Triangle extends Shape {
+    double side;
+
+    Triangle(double side) {
+        this.side = side;
+    }
+
+    @Override
+    double area() {
+        return ((Math.sqrt(3) / 4) * Math.pow(this.side, 2));
+    }
+
+    @Override
+    double perimeter() {
+        return (3 * this.side);
+    }
+
+    void displayMetrics() {
+        System.out.println(String.format("Side of Triangle = %.2f", this.side));
+        System.out.println(String.format("Perimeter of Triangle = %.2f", this.perimeter()));
+        System.out.println(String.format("Area of Triangle = %.2f", this.area()));
+        System.out.println();
     }
 }
 
 public class Program19_Shape {
+    public static void main(String[] args) {
+        Circle c = new Circle(10);
+        c.displayMetrics();
 
+        Rectangle r = new Rectangle(15, 20);
+        r.displayMetrics();
+
+        Triangle t = new Triangle(5);
+        t.displayMetrics();
+    }
 }
