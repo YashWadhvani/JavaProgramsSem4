68b988b1ad96d562a1cec75e8ebe25762e9d39a9
YashWadhvani <yashwadhvani123@gmail.com>
2024-01-12 14:21:44 +0530

    Program 20 Done.

---
+++ Program20_Product.java
@@ -0,0 +1,78 @@
+// Develop a simple online shopping system. Create a base class Product with attributes productId, productName, and price. Derive classes Electronics, Clothing, and Books from Product. Add specific attributes for each product type. Create objects for each class and demonstrate polymorphism by displaying product details.
+
+class Product {
+    int productID;
+    String productName;
+    double price;
+
+    Product(int productID, String productName, double price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    void displayInfo() {
+        System.out.println("\n");
+        System.out.println("Product ID = " + productID);
+        System.out.println("Product Name = " + productName);
+        System.out.println(String.format("Product Price = %.2f", price));
+    }
+}
+
+class Electronics extends Product {
+    String brand;
+
+    Electronics(int productID, String productName, double price, String brand) {
+        super(productID, productName, price);
+        this.brand = brand;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Product Brand = " + brand);
+    }
+}
+
+class Clothing extends Product {
+    String size;
+
+    Clothing(int productID, String productName, double price, String size) {
+        super(productID, productName, price);
+        this.size = size;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Product Size = " + size);
+    }
+}
+
+class Books extends Product {
+    String author;
+
+    Books(int productID, String productName, double price, String author) {
+        super(productID, productName, price);
+        this.author = author;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author = " + author);
+    }
+
+}
+
+public class Program20_Product {
+    public static void main(String[] args) {
+        Clothing Tee = new Clothing(9, "A's T", 999, "S");
+        Tee.displayInfo();
+
+        Books TLove = new Books(1, "Twisted Love", 300, "Ana Huang");
+        Books TGames = new Books(2, "Twisted Games", 400, "Ana Huang");
+        TLove.displayInfo();
+        TGames.displayInfo();
+    }
+}
