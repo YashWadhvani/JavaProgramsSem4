6e059289447829ffa3cfdebe7ac47b237d1ae242
YashWadhvani <yashwadhvani123@gmail.com>
2024-01-21 00:36:42 +0530

    Added New Methods with Menu.

--- Program21_BankAccounts.java
+++ Program21_BankAccounts.java
@@ -70,32 +70,77 @@ public class Program21_BankAccounts {
 
         while (true) {
             System.out.println("Bank Management System");
+            System.out.println("Enter Account Holder Name:");
+            Name = sc.next();
             System.out.println("1. Create Savings Account.\n2. Create Checking Account.");
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Enter Account Holder Name:");
-                    Name = sc.next();
-                    SavingsAccount SA = new SavingsAccount(Name);
-                    printMenu();
+                    SavingsAccount(Name);
                     break;
-
+                case 2:
+                    CheckingAccount(Name);
+                    break;
                 default:
+                    System.out.println("Invalid Choice! Please Enter a Valid Choice.");
                     break;
             }
         }
     }
 
-    private static void printMenu() {
+    private static void SavingsAccount(String Name) {
+        int amount;
+
+        SavingsAccount SA = new SavingsAccount(Name);
         int ch;
         while (true) {
-            System.out.println("1. Deposit.\2. Withdraw");
+            System.out.println("1. Deposit.\n2. Withdraw.\n3. Exit.");
             ch = sc.nextInt();
             switch (ch) {
                 case 1:
+                    System.out.println("Enter Amount to Deposit :-");
+                    amount = sc.nextInt();
+                    SA.deposit(amount, SA.accBalance);
+                    continue;
+                case 2:
+                    System.out.println("Enter Amount to Withdraw :-");
+                    amount = sc.nextInt();
+                    SA.withdraw(amount, SA.accBalance);
+                    continue;
+                case 3:
+                    System.out.println("Thank You for Using Bank Management System.");
                     break;
-
                 default:
+                    System.out.println("Invalid Choice! Please Enter a Valid Choice.");
+                    break;
+            }
+        }
+    }
+
+    private static void CheckingAccount(String Name) {
+        int amount;
+
+        CheckingAccount CA = new CheckingAccount(Name);
+        int ch;
+        while (true) {
+            System.out.println("1. Deposit.\n2. Withdraw.\n3. Exit.");
+            ch = sc.nextInt();
+            switch (ch) {
+                case 1:
+                    System.out.println("Enter Amount to Deposit :-");
+                    amount = sc.nextInt();
+                    CA.deposit(amount, CA.accBalance);
+                    break;
+                case 2:
+                    System.out.println("Enter Amount to Withdraw :-");
+                    amount = sc.nextInt();
+                    CA.withdraw(amount, CA.accBalance);
+                    continue;
+                case 3:
+                    System.out.println("Thank You for Using Bank Management System.");
+                    break;
+                default:
+                    System.out.println("Invalid Choice! Please Enter a Valid Choice.");
                     break;
             }
         }
