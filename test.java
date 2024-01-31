a24a56960d71ae58a31c911c07291288484ad960
YashWadhvani <yashwadhvani123@gmail.com>
2024-01-31 23:35:34 +0530

    Tried Driver Class for Bookstore Program.

---
+++ test.java
@@ -0,0 +1,9 @@
+import bookstore.*;
+
+public class test {
+
+    public static void main(String[] args) {
+        book TH = new bookstore.book("Helo", "World", 1234567890, 500, 5);
+        System.out.println(TH);
+    }
+}
