dee2511bf398be81c93febe07c6916a31ab35029
YashWadhvani <yashwadhvani123@gmail.com>
2024-04-01 00:39:28 +0530

    Program 33 Updated

--- Program33_StringBuffer.java
+++ Program33_StringBuffer.java
@@ -15,30 +15,23 @@ public class Program33_StringBuffer {
 
         s1 = new StringBuffer(str);
         s1.append(" Butterfly!");
-        // System.out.println(str);
         System.out.println("New String : " + s1 + "\n");
-        // System.out.println(s1);
 
         s2 = new StringBuffer(str);
         s2.delete(2, 3);
-        // System.out.println(str);
         System.out.println("New String : " + s2 + "\n");
-        // System.out.println(s2);
 
         s3 = new StringBuffer(str);
         s3.insert(1, str);
         System.out.println("New String : " + s3 + "\n");
-        // System.out.println(s3);
 
         s4 = new StringBuffer(str);
         s4.replace(2, 5, "yya!");
         System.out.println("New String : " + s4 + "\n");
-        // System.out.println(s4);
 
         s5 = new StringBuffer(str);
         s5.reverse();
         System.out.println("New String : " + s5 + "\n");
-        // System.out.println(s5);
 
     }
 }
